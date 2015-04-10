8e5caa407e55fa3829c3bad86474f7875160e6e5
ravenvn <ravenvn@yahoo.com.vn>
2015-04-11 00:43:05 +0700

    remove hook and other unnecessary files

--- client/startup.js
+++ client/startup.js
@@ -3,6 +3,5 @@ Meteor.startup(function() {
   	Deps.autorun(function() {
     	document.title = Session.get('documentTitle');
   	});
-  	Hooks.init();
 });
 Session.set('documentTitle', 'Quản lý số điện thoại khách hàng');
